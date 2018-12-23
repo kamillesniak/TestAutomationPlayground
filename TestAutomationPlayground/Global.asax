96785f095727224baede61e99c3305082df2f93e
kamillesniak <kamillesniak02@gmail.com>
2018-12-23 14:45:11 +0100

    Solution configuration

---
+++ TestAutomationPlayground/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestAutomationPlayground.MvcApplication" Language="C#" %>
---
+++ TestAutomationPlayground/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestAutomationPlayground.Startup))]
+namespace TestAutomationPlayground
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
